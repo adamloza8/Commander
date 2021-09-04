5cec7af9870125ade6be7492f20cfb359c04c278
adamloza8 <adamloza8@gmail.com>
2021-09-04 22:34:12 +0200

    Stage 2

--- Startup.cs
+++ Startup.cs
@@ -2,15 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Commander.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using MySql.Data.MySqlClient;
 
 namespace Commander
 {
@@ -28,6 +31,10 @@ namespace Commander
         {
 
             services.AddControllers();
+            services.AddScoped<ICommanderRepo, MockCommanderRepo>();
+            //services.AddTransient<MySqlConnection>(_ => new MySqlConnection(Configuration.GetConnectionString("CommanderConnection")));
+            services.AddDbContext<CommanderContext>(options=>options.UseMySQL(Configuration.GetConnectionString("CommanderConnection")));
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Commander", Version = "v1" });
